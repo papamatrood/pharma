cf094220a846cca9582e67a0d29e787ad3e03523
Papa Matrood <papamatrood@gmail.com>
2021-04-14 11:39:14 +0000

    Complétion des boutons stock faible et produits périmés ou en cours de péremption

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -15,24 +15,45 @@ class MainController extends AbstractController
     public function index(ProduitsRepository $produitsRepository, Request $request)
     {
         $products = $produitsRepository->findStockFaible();
+        $productsPerimes = $produitsRepository->findAllPerimes();
+        $productsEnCoursP = $produitsRepository->findAllEncours();
         
         $session = $request->getSession();
+
+        // Stock faible
         if (!$session->has('stockFaible')) {
             $session->set('stockFaible', false);
         }
         if (!$session->has('products')) {
             $session->set('products', []);
         }
-
         if (!empty($products)) {
             $session->set('stockFaible', true);
             $session->set('products', $products);
+        }
+
+        //Produits périmés ou en cours de péremption
+        if (!$session->has('productsPerimes') || !$session->has('productsEnCoursP')) {
+            $session->set('productsPerimes', false);
+            $session->set('productsEnCoursP', false);
+        }
+        if (!$session->has('productsP') || !$session->has('productsEnCours')) {
+            $session->set('productsP', []);
+            $session->set('productsEnCours', []);
+        }
+        if (!empty($productsPerimes)) {
+            $session->set('productsPerimes', true);
+            $session->set('productsP', $productsPerimes);
+        }
+        if (!empty($productsEnCoursP)) {
+            $session->set('productsEnCoursP', true);
+            $session->set('productsEnCours', $productsEnCoursP);
         }
         
         return $this->render('layout/layout.html.twig');
     }
 
-    
+    /*
     public function stockFaible(ProduitsRepository $produitsRepository, Request $request)
     {
         $products = $produitsRepository->findStockFaible();
@@ -52,4 +73,5 @@ class MainController extends AbstractController
             'products' => $products
         ]);
     }
+    */
 }
